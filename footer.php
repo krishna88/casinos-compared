10b989472d0641b25696fd734f53e5c2e3a92d4c
krishna <krishna88@gmail.com>
2014-08-17 21:07:32 +0100

    initiate

---
+++ footer.php
@@ -0,0 +1,26 @@
+        <!-- /.row -->
+       
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Casinos Compared</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ how-to-play.php
@@ -0,0 +1,69 @@
+
+<?php include 'header.php'; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="img/online-roulette.jpg" alt="Roulette Online How To?">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Online Roulette</h3>
+                <h4>How to Play?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+                <a class="btn btn-primary" href="#">Try Now<span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Two -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="img/online-blackjack.jpg" alt="Blackjack Online How to?">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Online Blackjack</h3>
+                <h4>How to Play?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
+                <a class="btn btn-primary" href="#">Try Now<span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Three -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="img/online-poker.jpg" alt="Poker Online How to?">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Online Poker</h3>
+                <h4>How to Play?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
+                <a class="btn btn-primary" href="#">Try Now <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+       
+ <?php include 'footer.php'; ?>
+        
